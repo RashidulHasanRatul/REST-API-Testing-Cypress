85ae8105ca3c115a49ac3d2be587c89c152ccfc3
Rashidul Hasan <rhratul12@gmail.com>
2022-07-28 02:55:09 +0600

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -5,6 +5,12 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    env: {
+      baseUrl: "https://gorest.co.in",
+      API_URL: "https://gorest.co.in/public/v2",
+      API_TOKEN:
+        "d43bf897a11bfac9c5543027f57d529840a15bc328c7c0fe64d303d6c088ba9b",
+    },
   },
   watchForFileChanges: false,
 });
