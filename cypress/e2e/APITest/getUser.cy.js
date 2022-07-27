1518633ca9013d1644259e89989b7cacc5ce7535
Rashidul Hasan <rhratul12@gmail.com>
2022-07-28 02:55:22 +0600

    Update getUser.cy.js

--- cypress/e2e/APITest/getUser.cy.js
+++ cypress/e2e/APITest/getUser.cy.js
@@ -4,11 +4,11 @@ describe("Get The User API Test", () => {
   it("Get the User", () => {
     cy.request({
       method: "GET",
-      url: "https://gorest.co.in/public/v2/posts",
+      url: Cypress.env("API_URL") + "/posts",
     }).then((response) => {
       cy.log(response.body[0].id);
       expect(response.status).to.eq(200);
-      expect(response.body[0].user_id).to.eq(2786);
+      
     });
   });
 });
